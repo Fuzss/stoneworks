4d4c2ce0d152309b8342fa87fefbf249fe3b4d55
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-21 19:55:58 +0100

    full 1.20.4 port

--- Common/src/main/java/fuzs/stoneworks/config/CommonConfig.java
+++ Common/src/main/java/fuzs/stoneworks/config/CommonConfig.java
@@ -5,7 +5,7 @@ import fuzs.puzzleslib.api.config.v3.ConfigCore;
 import fuzs.stoneworks.world.block.variant.StoneVariantsProvider;
 
 public class CommonConfig implements ConfigCore {
-    @Config(description = "Add vanilla stone block variants to the creative tab provided by Stoneworks.")
+    @Config(description = "Add vanilla stone block variants to the creative tab provided this mod.")
     public boolean vanillaVariantsInCreativeTab = true;
 
     @Override

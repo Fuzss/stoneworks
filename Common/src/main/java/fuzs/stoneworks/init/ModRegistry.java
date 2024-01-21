4ea9faa6329ebd2dacbe71d629f725e99f6063a3
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-21 19:22:20 +0100

    prepare 1.20.4 port

---
+++ Common/src/main/java/fuzs/stoneworks/init/ModRegistry.java
@@ -0,0 +1,27 @@
+package fuzs.stoneworks.init;
+
+import fuzs.puzzleslib.api.init.v2.RegistryManager;
+import fuzs.stoneworks.Stoneworks;
+import fuzs.stoneworks.world.block.variant.BlockVariant;
+import fuzs.stoneworks.world.block.variant.StoneBlockVariant;
+import fuzs.stoneworks.world.block.variant.StoneVariantsProvider;
+import net.minecraft.world.level.block.*;
+
+public class ModRegistry {
+    static final RegistryManager REGISTRY = RegistryManager.instant(Stoneworks.MOD_ID);
+
+    public static void touch() {
+        for (StoneBlockVariant variant : StoneVariantsProvider.getStoneBlockVariants().toList()) {
+            if (variant.blockVariant() == BlockVariant.PILLAR) {
+                REGISTRY.registerBlockItem(REGISTRY.registerBlock(variant.name(), () -> new RotatedPillarBlock(variant.baseBlockProperties())));
+            } else {
+                REGISTRY.registerBlockItem(REGISTRY.registerBlock(variant.name(), () -> new Block(variant.baseBlockProperties())));
+                if (variant.blockVariant().supportsAdditionalBlocks()) {
+                    REGISTRY.registerBlockItem(REGISTRY.registerBlock(variant.stairsName(), () -> new StairBlock(variant.baseBlockState(), variant.baseBlockProperties())));
+                    REGISTRY.registerBlockItem(REGISTRY.registerBlock(variant.slabName(), () -> new SlabBlock(variant.baseBlockProperties())));
+                    REGISTRY.registerBlockItem(REGISTRY.registerBlock(variant.wallName(), () -> new WallBlock(variant.baseBlockProperties())));
+                }
+            }
+        }
+    }
+}
---
+++ NeoForge/src/main/java/fuzs/stoneworks/data/ModBlockTagsProvider.java
@@ -0,0 +1,25 @@
+package fuzs.stoneworks.data;
+
+import fuzs.puzzleslib.api.data.v1.AbstractTagProvider;
+import fuzs.stoneworks.world.block.variant.StoneBlockVariant;
+import fuzs.stoneworks.world.block.variant.StoneVariantsProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.data.event.GatherDataEvent;
+
+import java.util.Objects;
+
+public class ModBlockTagsProvider extends AbstractTagProvider.Blocks {
+
+    public ModBlockTagsProvider(GatherDataEvent evt, String modId) {
+        super(evt, modId);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        StoneVariantsProvider.getStoneBlockVariants().flatMap(StoneBlockVariant::allBlocks).forEach(this.tag(BlockTags.MINEABLE_WITH_PICKAXE)::add);
+        StoneVariantsProvider.getStoneBlockVariants().map(StoneBlockVariant::stairs).filter(Objects::nonNull).forEach(this.tag(BlockTags.STAIRS)::add);
+        StoneVariantsProvider.getStoneBlockVariants().map(StoneBlockVariant::slab).filter(Objects::nonNull).forEach(this.tag(BlockTags.SLABS)::add);
+        StoneVariantsProvider.getStoneBlockVariants().map(StoneBlockVariant::wall).filter(Objects::nonNull).forEach(this.tag(BlockTags.WALLS)::add);
+    }
+}

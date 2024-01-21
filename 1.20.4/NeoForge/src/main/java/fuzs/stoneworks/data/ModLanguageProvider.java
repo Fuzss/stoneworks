a3367f4ff3ffbace2fd211cc29a79c55dd5f3e9e
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-21 19:22:20 +0100

    prepare 1.20.4 port

---
+++ 1.20.4/NeoForge/src/main/java/fuzs/stoneworks/data/ModLanguageProvider.java
@@ -0,0 +1,30 @@
+package fuzs.stoneworks.data;
+
+import com.google.common.collect.Maps;
+import fuzs.puzzleslib.api.data.v1.AbstractLanguageProvider;
+import fuzs.stoneworks.Stoneworks;
+import fuzs.stoneworks.world.block.variant.StoneBlockVariant;
+import fuzs.stoneworks.world.block.variant.StoneVariantsProvider;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.data.event.GatherDataEvent;
+
+import java.util.Map;
+
+public class ModLanguageProvider extends AbstractLanguageProvider {
+
+    public ModLanguageProvider(GatherDataEvent evt, String modId) {
+        super(evt, modId);
+    }
+
+    @Override
+    protected void addTranslations() {
+        this.addCreativeModeTab(Stoneworks.MOD_NAME);
+        Map<Block, String> translations = Maps.newHashMap();
+        for (StoneBlockVariant variant : StoneVariantsProvider.getStoneBlockVariants().toList()) {
+            variant.addTranslations(translations);
+        }
+        for (Map.Entry<Block, String> entry : translations.entrySet()) {
+            this.add(entry.getKey(), entry.getValue());
+        }
+    }
+}

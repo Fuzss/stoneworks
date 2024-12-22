2ee101f2e841649a0c705377a28f7d178d384f4a
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-22 23:26:50 +0100

    add crafting table recipes

--- 1.21.3/Common/src/main/java/fuzs/stoneworks/data/ModRecipeProvider.java
+++ 1.21.3/Common/src/main/java/fuzs/stoneworks/data/ModRecipeProvider.java
@@ -4,6 +4,7 @@ import fuzs.puzzleslib.api.data.v2.AbstractRecipeProvider;
 import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
 import fuzs.stoneworks.world.block.variant.StoneBlockVariant;
 import fuzs.stoneworks.world.block.variant.StoneVariantsProvider;
+import net.minecraft.data.recipes.RecipeBuilder;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -23,12 +24,31 @@ public class ModRecipeProvider extends AbstractRecipeProvider {
             this.stonecutterResultFromBase(RecipeCategory.BUILDING_BLOCKS, variant.block(), baseBlock);
             if (variant.blockVariant().supportsAdditionalBlocks()) {
                 Ingredient ingredient = baseBlock != variant.block() ? Ingredient.of(baseBlock, variant.block()) :
-                        Ingredient.of(baseBlock);
+                        Ingredient.of(variant.block());
+                this.stair(RecipeCategory.BUILDING_BLOCKS, variant.stairs(), variant.block());
                 this.stonecutterResultFromBase(RecipeCategory.BUILDING_BLOCKS, variant.stairs(), ingredient);
+                this.slab(RecipeCategory.BUILDING_BLOCKS, variant.slab(), variant.block());
                 this.stonecutterResultFromBase(RecipeCategory.BUILDING_BLOCKS, variant.slab(), ingredient, 2);
+                this.wall(RecipeCategory.BUILDING_BLOCKS, variant.wall(), variant.block());
                 this.stonecutterResultFromBase(RecipeCategory.BUILDING_BLOCKS, variant.wall(), ingredient);
             }
         }
+    }
+
+    @Deprecated(forRemoval = true)
+    public void stair(RecipeCategory recipeCategory, ItemLike result, ItemLike material) {
+        this.stairBuilder(recipeCategory, result, Ingredient.of(material))
+                .unlockedBy(getHasName(material), this.has(material))
+                .save(this.output);
+    }
+
+    @Deprecated(forRemoval = true)
+    public RecipeBuilder stairBuilder(RecipeCategory recipeCategory, ItemLike result, Ingredient material) {
+        return this.shaped(recipeCategory, result, 4)
+                .define('#', material)
+                .pattern("#  ")
+                .pattern("## ")
+                .pattern("###");
     }
 
     @Deprecated(forRemoval = true)
